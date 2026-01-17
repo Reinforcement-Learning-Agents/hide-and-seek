6707a72d80b41a043ea6f30772ff8bb6a45e2d67
Zakria96 <YOUR_askani444@gmail.com>
2026-01-17 18:05:55 +0100

    Add Python training script placeholder

---
+++ TrainingScripts/train_agent.py
@@ -0,0 +1,5 @@
+# Training script placeholder for documentation purposes
+# Run training from command line:
+# mlagents-learn ml-agents_config.yaml --env Builds/Training/unity-ml-agents_hide-and-seek.exe --run-id hns_run1 --force --no-graphics --time-scale 20
+
+print("Use mlagents-learn to train the Hide-and-Seek agent.")
